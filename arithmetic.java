145dd53aeab4f57ea5a6af70369cab421713cc37
kanchan44 <v.92kanchan@gmail.com>
2018-07-11 10:04:15 +0530

    added index.html

---
+++ arithmetic.java
@@ -0,0 +1,18 @@
+class arithmetic
+{
+public static void main(String args[])
+{
+int x=10;
+int y=5;
+int sum=x+y;
+int difference=x-y;
+int product=x*y;
+int divide=x/y;
+int mod=x%y;
+System.out.println("sum: "+sum);
+System.out.println("Subtract: "+difference);
+System.out.println("Product: "+product);
+System.out.println("Remainder: "+mod);
+System.out.println("divide: "+divide);
+}
+}
